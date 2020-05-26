137d13b55e6734f6360aef02f1bbb222dcc1c45d
xuzhiquan <2901239731@qq.com>
2020-05-26 21:38:29 +0800

    添加了部分注解

--- ImageView/ImageView.m
+++ ImageView/ImageView.m
@@ -32,6 +32,22 @@ static const CGSize imageSize = {100, 100};
             //2.开启图形上下文
             UIGraphicsBeginImageContextWithOptions(imageSize, YES, 0);
             
+            //创建一个基于位图的上下文（context）,并将其设置为当前上下文(context)
+            /*
+             函数原型为：
+
+             void UIGraphicsBeginImageContextWithOptions(CGSize size, BOOL opaque, CGFloat scale);
+
+             size——同UIGraphicsBeginImageContext
+
+             opaque—透明开关，如果图形完全不用透明，设置为YES以优化位图的存储。
+
+             scale—–缩放因子 iPhone 4是2.0，其他是1.0。虽然这里可以用[UIScreen mainScreen].scale来获取，但实际上设为0后，系统就会自动设置正确的比例了。
+             ————————————————
+             版权声明：本文为CSDN博主「larry-1991」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。
+             原文链接：https://blog.csdn.net/larryluoshuai/article/details/78200434
+             */
+            
             // 将图像画到当前的 image context 里，此时就完成了解压缩和重采样的工作
             
             //3.绘制到图形上下文中
